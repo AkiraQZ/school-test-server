0358453dfba1b20b143a14a2e62c3318645931cd
akiraqz <vboyrkin@gmail.com>
2024-05-30 09:04:11 +0800

    feat: add deletions of files from usage

--- src/router/fileActionsRouter.js
+++ src/router/fileActionsRouter.js
@@ -4,6 +4,7 @@ const { upload } = require('../databases/fileActions');
 const { generateCodes } = require('../scripts/generateCodes');
 const { getResults } = require('../scripts/getResults');
 const path = require('path');
+const fs = require('fs')
 
 router.post('/', (req, res, next) => {
     upload(req, res, (err) => {
@@ -17,16 +18,35 @@ router.post('/', (req, res, next) => {
 });
 
 router.get('/results', async (req, res) => {
+
+    function deleteResults(userId) {
+        const resultsDir = path.join(__dirname, '../../results/');
+    
+        fs.readdir(resultsDir, (err, files) => {
+            if (err) throw err;
+    
+            files.forEach(file => {
+                if (file.startsWith(`${userId}`) &&!file.endsWith('.xlsx')) {
+                    fs.unlink(path.join(uploadDir, file), err => {
+                        if (err) throw err;
+                        console.log(`Удалено ${file} из директории upload.`);
+                    });
+                }
+            });
+        });
+    }
+
     const fileName = req.query.userId;
     const filePath = path.join(__dirname, `../../results/${fileName}.xlsx`);
 
     try {
-        await getResults(req.query.userId);
+        await getResults(fileName);
         res.download(filePath, (err) => {
             if (err) {
                 console.error('Ошибка при скачивании файла:', err);
                 res.status(500).send('Ошибка при скачивании файла');
             } else {
+                deleteResults(fileName)
                 console.log('Файл успешно скачан');
             }
         });
@@ -36,7 +56,42 @@ router.get('/results', async (req, res) => {
     }
 });
 
+
+
 router.get('/codes', async (req, res) => {
+
+    function deleteUploadAndCodes(userId) {
+        const uploadDir = path.join(__dirname, '../../upload/');
+        const codesDir = path.join(__dirname, '../../codes/');
+    
+        fs.readdir(uploadDir, (err, files) => {
+            if (err) throw err;
+    
+            files.forEach(file => {
+                if (file.startsWith(`${userId}`) &&!file.endsWith('.xlsx')) {
+                    fs.unlink(path.join(uploadDir, file), err => {
+                        if (err) throw err;
+                        console.log(`Удалено ${file} из директории upload.`);
+                    });
+                }
+            });
+        });
+    
+        fs.readdir(codesDir, (err, files) => {
+            if (err) throw err;
+    
+            files.forEach(file => {
+                if (file.startsWith(`${userId}`) &&!file.endsWith('.xlsx')) {
+                    fs.unlink(path.join(codesDir, file), err => {
+                        if (err) throw err;
+                        console.log(`Удалено ${file} из директории codes.`);
+                    });
+                }
+            });
+        });
+    }
+    
+
     const fileName = req.query.userId;
     const filePath = path.join(__dirname, `../../codes/${fileName}.xlsx`);
 
@@ -46,6 +101,7 @@ router.get('/codes', async (req, res) => {
                 console.error('Ошибка при скачивании файла:', err);
                 res.status(500).send('Ошибка при скачивании файла');
             } else {
+                deleteUploadAndCodes(fileName);
                 console.log('Файл успешно скачан');
             }
         });
